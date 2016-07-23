cba749886bae5d614cbd968171052c51e8a28e2d
ashpad0502 <ashpad0502@gmail.com>
2016-07-23 20:47:38 +0530

    Add files via upload
    Selection Sort

---
+++ Selectionsort.java
@@ -0,0 +1,22 @@
+package selectionsort;
+
+public class Selectionsort
+{
+ public int[] selectionsort(int arr[], int n)
+ {
+	 int min,temp;
+	 for(int i=0;i<n-1;i++)
+	{   
+		 min = i;
+		for(int j=i+1;j<n;j++)
+		{ 
+		  if(arr[j]<arr[i])
+			min = j;  
+		}
+		 temp = arr[i];
+		 arr[i] = arr[min];
+		 arr[min] = temp;
+	}
+	return arr;
+ }
+}
---
+++ SelectionsortApp.java
@@ -0,0 +1,21 @@
+package selectionsort;
+import java.util.*;
+public class SelectionsortApp 
+{
+ public static void main(String args[])
+ {
+  int arr[] = new int[10];
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the array size <10: ");
+  int n=sc.nextInt();
+  System.out.println("Enter array elements: ");
+  for(int i=0;i<n;i++)
+   arr[i]=sc.nextInt();
+  Selectionsort ss = new Selectionsort();
+  int sorted [] = ss.selectionsort(arr, n);
+  System.out.println("Sorted array after Selection Sort: ");
+  for(int i=0;i<n;i++)
+  System.out.print(sorted[i]+" ");
+  sc.close();
+ }
+}
